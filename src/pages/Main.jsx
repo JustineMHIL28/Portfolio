72250b17089407862a785f1f10bbc6f8feebc9ac
JustineMHIL1996 <justinemhil1996@gmail.com>
2024-10-19 07:19:17 +0800

    Initial commit

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -56,7 +56,7 @@ export const MainPage = () => {
               <p>
                 I’m Hilario Justine M., a 27-year-old Web Developer from Malolos, Bulacan. With a passion for technology, I specialize in building efficient and reliable web applications. I have experience in various technologies, including HTML, CSS, JavaScript, and ReactJS, allowing me to create dynamic user experiences.
               </p>
-              <p style={{marginTop: '5px'}}>
+              <p style={{marginTop: '10px'}}>
                 I thrive on continuous learning and enjoy collaborating with others to deliver high-quality projects. When I'm not coding, I love exploring new tech trends and sharing insights with fellow developers.
               </p>
             </>
@@ -84,7 +84,7 @@ export const MainPage = () => {
               <p>
                 I have hands-on experience with a variety of technologies that empower me to create dynamic and efficient web applications. Some of the key technologies 
               </p>
-              <p style={{marginTop: '20px'}}>
+              <p style={{marginTop: '10px'}}>
                  I've worked with include JavaScript, which allows me to build interactive elements; React, a powerful library for building user interfaces; Redux for state management; Node.js for server-side development; and RESTful APIs that facilitate seamless communication between the client and server. Additionally, I continuously explore emerging technologies to enhance my skill set and deliver innovative solutions.
              </p>
             </>
@@ -112,7 +112,7 @@ export const MainPage = () => {
               <p>
                 Here are a few projects I've had the opportunity to work on, showcasing my skills and expertise in web development.
               </p>
-              <p style={{marginTop: '5px'}}>
+              <p style={{marginTop: '10px'}}>
                 Each project reflects my commitment to creating efficient and user-friendly applications that meet the needs of clients and users alike.
              </p>
             </>
@@ -140,7 +140,7 @@ export const MainPage = () => {
               <p>
                 Here’s a quick rundown of my work experience, highlighting the roles I've held and the skills I've developed over the years.
               </p>
-              <p style={{marginTop: '5px'}}>
+              <p style={{marginTop: '10px'}}>
                 Each position has contributed to my growth as a web developer and equipped me with the necessary tools to tackle complex challenges in the tech industry.
              </p>
             </>
