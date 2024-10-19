d7f9cc1db63021c1d8c11860c0e68c5d2d33f886
JustineMHIL1996 <justinemhil1996@gmail.com>
2024-10-19 09:25:48 +0800

    test

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -2,55 +2,53 @@ import '../styles/InfoCard/InfoCard-Responsive.scss';
 import React from "react";
 import { Image } from "antd";
 
-// Renders a card with images, names, durations, and descriptions based on provided props
 const renderCard = (
     { style, type, disableDataImage, disableDataSubImage, disableDataDescription, disableLink, disableTooltips },
     { name, description, image, subimage, duration, link }
 ) => (
     <div style={style} className={`infoCard${type}Page`}>
         <div className={`group${type}Page`}>
-            {/* Render main image with link or as a button if link is disabled */}
             {!disableDataImage && (
-                disableLink ? (
-                    <button className={`${type}ImageButton`}>
-                        <Image className={`${type}Image`} src={image} preview={false} />
-                        {!disableTooltips && <div className="tooltip">{name}</div>}
-                    </button>
-                ) : (
-                    <a href={link} className={`${type}ImageLink`}>
-                        <Image className={`${type}Image`} src={image} preview={false} />
-                        {!disableTooltips && <div className="tooltip">{name}</div>}
-                    </a>
-                )
+                <>
+                    {!disableLink ? (
+                        <a href={link}>
+                            <Image className={`${type}Image`} src={image} preview={false} />
+                            {!disableTooltips && <div className="tooltip">{name}</div>}
+                        </a>
+                    ) : (
+                        <>
+                            <Image className={`${type}Image`} src={image} preview={false} />
+                            {!disableTooltips && <div className="tooltip">{name}</div>}
+                        </>
+                    )}
+                </>
             )}
-            {/* Display name and duration */}
             <div className={`${type}Name`}>
                 {name}
                 {duration && <div className={`${type}Date`}>{duration}</div>}
             </div>
         </div>
-        
-        {/* Render subimage with link or as a button if link is disabled */}
         {!disableDataSubImage && (
-            disableLink ? (
-                <button className={`${type}SubImageButton`}>
-                    <Image className={`${type}SubImage`} src={subimage} preview={false} />
-                    {!disableTooltips && <div className="tooltip">{name}</div>}
-                </button>
-            ) : (
-                <a href={link} className={`${type}SubImageLink`}>
-                    <Image className={`${type}SubImage`} src={subimage} preview={false} />
-                    {!disableTooltips && <div className="tooltip">{name}</div>}
-                </a>
-            )
+            <>
+                {!disableLink ? (
+                    <a href={link}>
+                        <Image className={`${type}SubImage`} src={subimage} preview={false} />
+                        {!disableTooltips && <div className="tooltip">{name}</div>}
+                    </a>
+                ) : (
+                    <>
+                        <Image className={`${type}SubImage`} src={subimage} preview={false} />
+                        {!disableTooltips && <div className="tooltip">{name}</div>}
+                    </>
+                )}
+            </>
         )}
-        
-        {/* Display description if not disabled */}
-        {!disableDataDescription && <div className={`${type}Desc`}>{description}</div>}
+        {!disableDataDescription && (
+            <div className={`${type}Desc`}>{description}</div>
+        )}
     </div>
 );
 
-// Expose the renderCard function as INFOCARD.main
 export const INFOCARD = {
     main: renderCard,
 };
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -51,7 +51,16 @@ export const MainPage = () => {
           title: STRING.overviewTitle,
           subtitle: "INTRODUCTION",
           disableSubtitle: false,
-          description: '',
+          description: (
+            <>
+              <>
+                I’m Hilario Justine M., a 27-year-old Web Developer from Malolos, Bulacan. With a passion for technology, I specialize in building efficient and reliable web applications. I have experience in various technologies, including HTML, CSS, JavaScript, and ReactJS, allowing me to create dynamic user experiences.
+              </>
+              <p style={{marginTop: '10px'}}>
+                I thrive on continuous learning and enjoy collaborating with others to deliver high-quality projects. When I'm not coding, I love exploring new tech trends and sharing insights with fellow developers.
+              </p>
+            </>
+          ),
           disableDescription: false,
           data: originalData.overviewData || [],
           disableDataImage: false,
@@ -70,7 +79,16 @@ export const MainPage = () => {
           title: STRING.technologiesTitle,
           subtitle: "MY SKILLS",
           disableSubtitle: false,
-          description: '',
+          description: (
+            <>
+              <p>
+                I have hands-on experience with a variety of technologies that empower me to create dynamic and efficient web applications. Some of the key technologies 
+              </p>
+              <p style={{marginTop: '10px'}}>
+                 I've worked with include JavaScript, which allows me to build interactive elements; React, a powerful library for building user interfaces; Redux for state management; Node.js for server-side development; and RESTful APIs that facilitate seamless communication between the client and server. Additionally, I continuously explore emerging technologies to enhance my skill set and deliver innovative solutions.
+             </p>
+            </>
+          ),
           disableDescription: false,
           data: originalData.technologiesData || [],
           disableDataImage: false,
@@ -89,7 +107,16 @@ export const MainPage = () => {
           title: STRING.projectsTitle,
           subtitle: "PROJECTS",
           disableSubtitle: true,
-          description: '',
+          description: (
+            <>
+              <p>
+                Here are a few projects I've had the opportunity to work on, showcasing my skills and expertise in web development.
+              </p>
+              <p style={{marginTop: '10px'}}>
+                Each project reflects my commitment to creating efficient and user-friendly applications that meet the needs of clients and users alike.
+             </p>
+            </>
+          ),
           disableDescription: false,
           data: originalData.projectsData || [],
           disableDataImage: true,
@@ -108,7 +135,16 @@ export const MainPage = () => {
           title: STRING.careerTitle,
           subtitle: "WORK EXPERIENCE",
           disableSubtitle: false,
-          description: '',
+          description: (
+            <>
+              <p>
+                Here’s a quick rundown of my work experience, highlighting the roles I've held and the skills I've developed over the years.
+              </p>
+              <p style={{marginTop: '10px'}}>
+                Each position has contributed to my growth as a web developer and equipped me with the necessary tools to tackle complex challenges in the tech industry.
+             </p>
+            </>
+          ),
           disableDescription: false,
           data: originalData.careerData || [],
           disableDataImage: false,
