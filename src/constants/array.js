06c3a18903c1edd441d198984e1daca76287f2e5
JustineMHIL1996 <JusinteMHil1996@gmail.com>
2025-03-28 17:45:05 -0800

    new

--- src/constants/array.js
+++ src/constants/array.js
@@ -12,7 +12,7 @@ export const ARRAY = {
                     <p>
                         I’m Justine M. Hilario, a 28 yrs old Web Developer with a passion for technology. I specialize in building efficient and reliable web applications, using various technologies such as HTML, CSS, JavaScript and ReactJS to create dynamic and user-friendly experiences.
                     </p>
-                    <p style={{ marginTop: '10px' }}>
+                    <p style={{ marginTop: '8px' }}>
                         thrive on continuous learning and enjoy collaborating with others to deliver high-quality projects. When I'm not coding, I explore new tech trends and share insights with fellow developers.
                     </p>
                 </>
@@ -23,9 +23,9 @@ export const ARRAY = {
             type: 'technologies',
             content: (
                 <>
-                    <p>I have hands-on experience with a variety of technologies that empower me to create dynamic and efficient web applications. Some of the key technologies</p>
-                    <p style={{ marginTop: '10px' }}>
-                        I've worked with include HTML, CSS, Antd, React, Node.js, JS, TS and RESTful APIs. I continuously explore emerging technologies to enhance my skill set and deliver innovative solutions.
+                    <p>I have extensive hands-on experience with a variety of technologies that enable me to create dynamic and efficient web applications. Some of the key technologies I have worked with include HTML, CSS, Antd, React, Node.js, JavaScript, TypeScript, and RESTful APIs.</p>
+                    <p style={{ marginTop: '8px' }}>
+                        I continuously explore emerging technologies to expand my skill set and deliver innovative and effective solutions in the field of web development.
                     </p>
                 </>
             ),
@@ -35,9 +35,12 @@ export const ARRAY = {
             type: 'projects',
             content: (
                 <>
-                    <p>Here are a few projects I've had the opportunity to work on, showcasing my skills and expertise in web development.</p>
-                    <p style={{ marginTop: '10px' }}>
-                        Each project reflects my commitment to creating efficient and user-friendly applications that meet the needs of clients and users alike.
+                    <p>Here are some of the projects I have had the opportunity to work on, showcasing my skills and expertise in web development.</p>
+                    <p style={{ marginTop: '8px' }}>
+                        EWith each project I’ve taken on, I have focused not only on building well-designed and functional applications but also on ensuring efficiency, speed, and ease of use for end users. Each project reflects my dedication to delivering high-quality digital solutions that meet the needs of both clients and users
+                    </p>
+                    <p style={{ marginTop: '8px' }}>
+                        Over time, I have gained experience in developing dynamic websites, optimizing web application performance, and utilizing various technologies to enhance functionality and user experience. These projects have allowed me to refine my problem-solving skills, experiment with innovative solutions, and stay updated with the latest trends in the ever-evolving field of web development.
                     </p>
                 </>
             ),
@@ -47,9 +50,12 @@ export const ARRAY = {
             type: 'career',
             content: (
                 <>
-                    <p>Here’s a quick rundown of my work experience, highlighting the roles I've held and the skills I've developed over the years.</p>
-                    <p style={{ marginTop: '10px' }}>
-                        Each position has contributed to my growth as a web developer and equipped me with the necessary tools to tackle complex challenges in the tech industry.
+                    <p>Here’s an in-depth look at my work experience, showcasing the various roles I’ve taken on and the valuable skills I’ve cultivated throughout my career.</p>
+                    <p style={{ marginTop: '8px' }}>
+                        Over the years, I have had the opportunity to work on diverse projects, collaborate with talented professionals, and continuously refine my expertise in web development. Each position I’ve held has played a crucial role in shaping my technical proficiency, problem-solving abilities, and adaptability in a fast-evolving industry.
+                    </p>
+                    <p style={{ marginTop: '8px' }}>
+                        As I continue to grow as a web developer, I remain committed to staying up-to-date with industry trends, learning new skills, and pushing the boundaries of what’s possible in the ever-evolving world of technology.
                     </p>
                 </>
             ),
